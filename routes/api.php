cb0e42ff73b27abac95ff163a87d5bc1af6428f7
PabloChDAW <pcg0031@alu.medac.es>
2025-03-26 20:10:33 +0100

    Añadir rutas de registro, login y logout
    - Comprobadas con Postman. Actualmente devuelven textos de prueba.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PointController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,10 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// Las rutas necesarias para un CRUD completo (comprobar con `php artisan route:list`)
 Route::apiResource('points', PointController::class);
+
+// Rutas para la autenticación con Sanctum
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
