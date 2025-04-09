11645844d8027873fafbcb5de63bdf923036c448
javi19xx <fsc0005@alu.medac.es>
2025-04-09 20:04:18 +0200

    OrderController sin Hacer
    Falta casi todo de orderController y productController, además, hay que hacer las políticas de cada uno

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,33 +5,55 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controllers\Middleware;
 
 class OrderController extends Controller
 {
+
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum')
+        ];
+    }
+    
+     /**
+     * Muestra todos los pedidos almacenados en la base de datos.
+     */
     public function index()
     {
         $orders = Auth::user()->orders;
         return response()->json($orders);
     }
 
+    /**
+     * Almacena un nuevo pedido asociado a un usuario.
+     */
     public function store(Request $request)
     {
         $validated = $request->validate([
             'total' => 'required|numeric|min:0',
-            'status' => 'nullable|string|max:255',
+            'status' => 'nullable|string|max:20',
+            'address' => 'required|string|max:50',
         ]);
 
         $order = Auth::user()->orders()->create($validated);
+        
 
         return response()->json($order, 201);
     }
-
+    /**
+     * Muestra los datos de un pedido específico.
+     */
     public function show(Order $order)
     {
         $this->authorizeOrder($order);
         return response()->json($order);
     }
 
+    /**
+     * Actualiza los datos de un punto específico.
+     */
     public function update(Request $request, Order $order)
     {
         $this->authorizeOrder($order);
