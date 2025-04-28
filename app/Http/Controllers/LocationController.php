6fe567bf1a65bfc43c9b5a99442ca28d1d9e29d4
PabloChDAW <pcg0031@alu.medac.es>
2025-04-28 12:17:40 +0200

    Implementar store() en LocationController.php
    Valida los campos introducidos por el usuario y añade el user_id del usuario autenticado.

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LocationController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'street' => 'required',
+            'building' => 'required',
+            'area' => 'required',
+        ]);
+
+        Location::create([
+            'street' => $request->street,
+            'building' => $request->building,
+            'area' => $request->area,
+            'user_id' => Auth::id(),
+        ]);
+    }
 }
