15ec15a8a6a2b52407cda736b9e96737e2d16328
JesusGomez99 <jgl0033@alu.medac.es>
2025-05-20 18:17:22 +0200

    Control del request en PaypalController

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -8,10 +8,19 @@ class PayPalController extends Controller
 {
     public function paymentCompleted(Request $request)
     {
+
         $details = $request->input('details');
+        $cart = $request->input('cart');
 
         // Aquí puedes guardar el pedido en la base de datos, enviar confirmación, etc.
-        Log::info('Pago recibido de PayPal', $details);
+        // Muestra lo que llegó al backend
+        Log::info('Datos recibidos de PayPal:', [
+            'details' => $details
+        ]);
+
+        Log::info('Pedido Realizado:', [
+            'cart' => $cart
+        ]);
 
         return response()->json(['status' => 'success']);
     }
