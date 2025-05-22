62f0f30c9a92a191cd02be44f8f631dcfb93ab1e
PabloChDAW <pcg0031@alu.medac.es>
2025-05-22 11:42:45 +0200

    Update cors.php
    TODO - Añadir URL del cliente

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')], // TODO meter la URL del cliente aquí
 
     'allowed_origins_patterns' => [],
 
